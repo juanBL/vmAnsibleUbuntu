40f79b7e681b24c9e30343e6230c9832be0b0632
Pablo Ros <pablo.ros@promofarma.com>
2017-01-24 17:23:10 +0100

    Add shell script to provision vagrant with Ansible.

---
+++ vagrant/shell/vagrant_main_provision.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+if ! ansible --version | grep ansible;
+then
+    echo "-> Installing Ansible"
+    # Add Ansible Repository & Install Ansible
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get -y install ansible
+else
+        echo "-> Ansible already Installed!"
+fi
+
+# Install Ansible Galaxy modules
+# To review in furure: http://docs.ansible.com/ansible/galaxy.html#id12
+echo "-> Installing Ansibe Galaxy Modules"
+roles_list[0]='geerlingguy.mysql'
+
+for role_and_version in "${roles_list[@]}"
+do
+    role_and_version_for_grep="${role_and_version/,/, }"
+
+    if ! sudo ansible-galaxy list | grep -qw "$role_and_version_for_grep";
+    then
+            echo "Installing ${role_and_version}"
+            sudo ansible-galaxy -f install $role_and_version
+    else
+        echo "Already installed ${role_and_version}"
+    fi
+done
+
+# Execute Ansible
+echo "-> Execute Ansible"
+ansible-playbook /ansible/playbook_development.yml -i /ansible/inventories/development --connection=local
